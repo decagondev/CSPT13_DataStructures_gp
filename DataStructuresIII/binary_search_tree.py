460d91594b947c9610dc3677e7386227f5b0febd
Tom Tarpey <tomtarpeydev@gmail.com>
2020-09-22 04:12:01 +0100

    BST Plan

--- DataStructuresIII/binary_search_tree.py
+++ DataStructuresIII/binary_search_tree.py
@@ -9,6 +9,11 @@ This part of the project comprises two days:
    on the BSTNode class.
 """
 class BSTNode:
+    """
+    The left subtree of a node contains only nodes with values lesser than the node’s value.
+    The right subtree of a node contains only nodes with values greater than or equal to the node’s value.
+    The left and right subtree each must also be a binary search tree.
+    """
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -16,19 +21,70 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
+        # check if the new nodes value is less than the current nodes value
+            # if there is no left child already here
+                # add the new node to the left
+                # create a BSTNode and encapsulate the value in it then set it to the left
+            # otherwise call insert on the left node
+        # otherwise (the new nodes value is greaterthan or equal to the current node value)
+            # if there is no right child already here
+                # add the new node to the right
+                # create a BSTNode and encapsulate the value in it then set it to the right
+            # otherwise call insert on the right node
         pass
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        # if the value of the current node matches the target
+            # return True
+
+        # check if the target is less than the current nodes value
+            # if there is no left child already here
+                # return False
+            # otherwise
+                # return a call of contains on the left child passing in the target value
+        # otherwise (the target is greater than the current nodes value)
+            # if there is no right child already here
+                # return False
+            # otherwise
+                # return a call of contains on the right child passing in the target value
         pass
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # check for an empty tree
+            # return None
+
+        # ----------------------------------------------
+        # recursive approach
+        # check if there is no node to the right
+            # return the nodes value
+        # return a call to get max on the right child
+        # -----------------------------------------------
+
+        # iterative aproach
+
+        # initialise the max value
+
+        # get a ref to the current node
+
+        # loop while there is still a current node
+            # if the current value is greater than the max value, update the max value
+            # move on to the next right node
+        
+        # return the max value
         pass
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        # call the function passing in the current nodes value
+
+        # if there is a node to the left
+            # call the function on the left value
+        
+        # if there is a node to the right
+            # call the function on the right node
         pass
 
     # Part 2 -----------------------
